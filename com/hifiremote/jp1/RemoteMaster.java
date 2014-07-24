72a4f00eadde890878886344d19cb954ac237bec
yawor <yawor@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2014-07-24 06:27:14 +0000

    - Updated version to v2.03 Alpha 24 Test 6b (Marcin)
    git-svn-id: svn://svn.code.sf.net/p/controlremote/code/trunk/km@1250 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- com/hifiremote/jp1/RemoteMaster.java
+++ com/hifiremote/jp1/RemoteMaster.java
@@ -108,7 +108,7 @@ public class RemoteMaster extends JP1Frame implements ActionListener, PropertyCh
   private static JP1Frame frame = null;
 
   /** Description of the Field. */
-  public final static String version = "v2.03 Alpha 24 Test 6";
+  public final static String version = "v2.03 Alpha 24 Test 6b";
 
   public enum Use
   {
