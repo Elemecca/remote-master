233d3a2577d41fea5248d7e78f943324e09be959
mathsdon <mathsdon@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2014-08-13 15:58:53 +0000

    - Update to segment panel, still not complete (Graham)
    git-svn-id: svn://svn.code.sf.net/p/controlremote/code/trunk/km@1265 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- com/hifiremote/jp1/SegmentPanel.java
+++ com/hifiremote/jp1/SegmentPanel.java
@@ -2,26 +2,17 @@ package com.hifiremote.jp1;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.UIDefaults;
-import javax.swing.table.TableCellRenderer;
 
 public class SegmentPanel extends RMPanel
 {
   private SegmentTableModel model = null;
   private JP1Table table = null;
-  private Font lblFont = ( new JLabel() ).getFont();
-  private List< List< Integer > > rowColHeight = new ArrayList< List< Integer > >();
   
   public SegmentPanel()
   {    
@@ -45,6 +36,7 @@ public class SegmentPanel extends RMPanel
       RemoteMaster rm = remoteConfig.getOwner();
       List< Segment > segments = new ArrayList< Segment >();
       short[] dataToShow = rm.useSavedData() ? remoteConfig.getSavedData() : remoteConfig.getData();
+      int base = remote.getBaseAddress();
       
       // first two bytes are checksum, and in XSight remotes next 18 bytes are E2 info
       int pos = remote.usesEZRC() || remote.usesSimpleset() ? 20 : 2;
@@ -55,12 +47,13 @@ public class SegmentPanel extends RMPanel
         segType = dataToShow[ pos + 2 ];
         int segFlags = dataToShow[ pos + 3 ];
         Hex segData = new Hex( dataToShow, pos + 4, segLength - 4 );
-        segments.add( new Segment( segType, segFlags, segData ) );
+        Segment seg = new Segment( segType, segFlags, segData );
+        seg.setAddress( base + pos );
+        segments.add( seg );
         pos += segLength;
       }
-      model.setData( segments );
+      model.set( remote, segments );
       table.initColumns();
-      table.getColumnModel().getColumn(4).setCellRenderer( new TextAreaRenderer() );
     }
   }
 
@@ -70,67 +63,6 @@ public class SegmentPanel extends RMPanel
     if ( model != null && l != null )
     {
       model.addPropertyChangeListener( l );
-    }
-  }
-  
-  private class TextAreaRenderer extends JTextArea implements TableCellRenderer
-  {
-    private Color selectionBackground = null;
-    private Color selectionForeground = null;
-    
-    public TextAreaRenderer() 
-    {
-      setLineWrap( true );
-      setWrapStyleWord( true );
-      setFont( lblFont );
-      UIDefaults defaults = javax.swing.UIManager.getDefaults();
-      selectionBackground = defaults.getColor("List.selectionBackground");
-      selectionForeground = defaults.getColor("List.selectionForeground");
-    }
-    
-    @Override
-    public Component getTableCellRendererComponent(JTable table, Object value, 
-        boolean isSelected, boolean hasFocus, int row, int col) 
-    {
-      String text = ( String )value;
-      setText( text );
-      adjustRowHeight( table, row, col );
-      setBackground( isSelected ? selectionBackground : Color.WHITE );
-      setForeground( isSelected ? selectionForeground : Color.BLACK );
-      return this;
-    }
-    
-    private void adjustRowHeight(JTable table, int row, int column) {
-      //The trick to get this to work properly is to set the width of the column to the
-      //textarea. The reason for this is that getPreferredSize(), without a width tries
-      //to place all the text in one line. By setting the size with the with of the column,
-      //getPreferredSize() returns the proper height which the row should have in
-      //order to make room for the text.
-      int cWidth = table.getTableHeader().getColumnModel().getColumn( column ).getWidth();
-      setSize( new Dimension( cWidth, 1000 ) );
-      int prefH = getPreferredSize().height;
-      while ( rowColHeight.size() <= row ) 
-      {
-        rowColHeight.add( new ArrayList<Integer>( column ) );
-      }
-      List<Integer> colHeights = rowColHeight.get( row );
-      while ( colHeights.size() <= column ) 
-      {
-        colHeights.add( 0 );
-      }
-      colHeights.set( column, prefH );
-      int maxH = prefH;
-      for ( Integer colHeight : colHeights ) 
-      {
-        if ( colHeight > maxH ) 
-        {
-          maxH = colHeight;
-        }
-      }
-      if ( table.getRowHeight(row) != maxH ) 
-      {
-        table.setRowHeight( row, maxH );
-      }
     }
   }
 }
--- com/hifiremote/jp1/SegmentTableModel.java
+++ com/hifiremote/jp1/SegmentTableModel.java
@@ -1,29 +1,44 @@
 package com.hifiremote.jp1;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.AbstractCellEditor;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.UIDefaults;
+import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 
 public class SegmentTableModel extends JP1TableModel< Segment >
 {
   List< Segment > segments = null;
+  Remote remote = null;
   
   public SegmentTableModel()
   {}
   
+  private static Font lblFont = ( new JLabel() ).getFont();
+  
   private static final String[] colNames =
   {
-    "    ", "Length", "Type", "Flags", "Data"
+    "    ", "Address", "Length", "Type", "Flags", "Data"
   };
 
   private static final String[] colPrototypeNames =
   {
-    " 00 ", "Length_", "Type_", "Flags_", "Data___________________________"
+    " 00 ", "Address_", "Length_", "Type_", "Flags_", "Data___________________________"
   };
 
   private static final Class< ? >[] colClasses =
   {
-    Integer.class, String.class, String.class, String.class, Hex.class
+    Integer.class, String.class, String.class, String.class, String.class, Hex.class
   };
 
   @Override
@@ -53,7 +68,7 @@ public class SegmentTableModel extends JP1TableModel< Segment >
   @Override
   public boolean isColumnWidthFixed( int col )
   {
-    return col != 4;
+    return col != 5;
   }
   
   @Override
@@ -61,18 +76,35 @@ public class SegmentTableModel extends JP1TableModel< Segment >
   {
     if ( col == 0 )
       return new RowNumberRenderer();
+    else if ( col == 5 )
+    {
+      return new TextAreaRenderer();
+    }
+    return null;
+  }
+  
+  @Override
+  public TableCellEditor getColumnEditor( int col )
+  {
+    if ( col == 5 )
+    {
+      return new TextAreaEditor();
+    }
     return null;
   }
   
   @Override
   public boolean isCellEditable( int row, int col )
   {
-    return false;
+    Segment seg = getRow( row );
+    return ( !remote.getSegmentTypes().contains( seg.get_Type() ) && col > 3 );
   }
   
-  public void set( List< Segment > segments )
+  public void set( Remote remote, List< Segment > segments )
   {
+    this.remote = remote;
     this.segments = segments;
+    setData( segments );
     fireTableDataChanged();
   }
 
@@ -85,15 +117,109 @@ public class SegmentTableModel extends JP1TableModel< Segment >
       case 0:
         return new Integer( row + 1 );
       case 1:
-        return String.format( "%04X", seg.getHex().length() + 4 );
+        return String.format( "%04X", seg.getAddress() );
       case 2:
-        return String.format( "%02X", seg.get_Type() );
+        return String.format( "%04X", seg.getHex().length() + 4 );
       case 3:
-        return String.format( "%02X", seg.getFlags() );
+        return String.format( "%02X", seg.get_Type() );
       case 4:
+        return String.format( "%02X", seg.getFlags() );
+      case 5:
         return seg.getHex().toString();
       default:
         return null;
+    }
+  }
+  
+  @Override
+  public void setValueAt( Object value, int row, int col )
+  {
+    Segment seg = getRow( row );
+    if ( col == 5 )
+    {
+      seg.setHex( ( Hex )value );
+    }
+  }
+  
+  private static class TextAreaRenderer extends JTextArea implements TableCellRenderer
+  {
+    private Color selectionBackground = null;
+    private Color selectionForeground = null;
+    private List< List< Integer > > rowColHeight = new ArrayList< List< Integer > >();
+    
+    public TextAreaRenderer() 
+    {
+      setLineWrap( true );
+      setWrapStyleWord( true );
+      setFont( lblFont );
+      UIDefaults defaults = javax.swing.UIManager.getDefaults();
+      selectionBackground = defaults.getColor("List.selectionBackground");
+      selectionForeground = defaults.getColor("List.selectionForeground");
+    }
+    
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, 
+        boolean isSelected, boolean hasFocus, int row, int col) 
+    {
+      String text = ( String )value;
+      setText( text );
+      adjustRowHeight( table, row, col );
+      setBackground( isSelected ? selectionBackground : Color.WHITE );
+      setForeground( isSelected ? selectionForeground : Color.BLACK );
+      return this;
+    }
+    
+    private void adjustRowHeight(JTable table, int row, int column) {
+      //The trick to get this to work properly is to set the width of the column to the
+      //textarea. The reason for this is that getPreferredSize(), without a width tries
+      //to place all the text in one line. By setting the size with the with of the column,
+      //getPreferredSize() returns the proper height which the row should have in
+      //order to make room for the text.
+      int cWidth = table.getTableHeader().getColumnModel().getColumn( column ).getWidth();
+      setSize( new Dimension( cWidth, 1000 ) );
+      int prefH = getPreferredSize().height;
+      while ( rowColHeight.size() <= row ) 
+      {
+        rowColHeight.add( new ArrayList<Integer>( column ) );
+      }
+      List<Integer> colHeights = rowColHeight.get( row );
+      while ( colHeights.size() <= column ) 
+      {
+        colHeights.add( 0 );
+      }
+      colHeights.set( column, prefH );
+      int maxH = prefH;
+      for ( Integer colHeight : colHeights ) 
+      {
+        if ( colHeight > maxH ) 
+        {
+          maxH = colHeight;
+        }
+      }
+      if ( table.getRowHeight(row) != maxH ) 
+      {
+        table.setRowHeight( row, maxH );
+      }
+    }
+  }
+  
+  private static class TextAreaEditor extends AbstractCellEditor implements TableCellEditor 
+  {
+    JComponent component = new JTextArea();
+
+    public Component getTableCellEditorComponent( JTable table, Object value, boolean isSelected,
+        int rowIndex, int vColIndex) 
+    {
+      JTextArea area = ( JTextArea ) component;
+      area.setFont( lblFont );
+      area.setText( ( String ) value);
+      area.setLineWrap( true );
+      area.setWrapStyleWord( true );
+      return component;
+    }
+
+    public Object getCellEditorValue() {
+      return ( new Hex( ( ( JTextArea )component ).getText() ) );
     }
   }
 }
