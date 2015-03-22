bbf3a8ce956644c152662baf535f07d02c68d02b
mathsdon <mathsdon@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2015-03-22 13:41:37 +0000

    - Further improvement to the Linux script Setup.sh (Graham)
    git-svn-id: svn://svn.code.sf.net/p/controlremote/code/trunk/km@1338 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- Setup.sh
+++ Setup.sh
@@ -11,10 +11,16 @@
 # Further, the user is added to the dialout group if not already a member.  This is needed in order to use
 # RMIR USB serial interfaces without root access.
 #
-# Created by Graham Dixon (mathdon), March 21, 2015.
+# Created by Graham Dixon (mathdon), March 22, 2015.
 
 # First identify the directory containing this script.
 here=$(dirname $(readlink -sf $0)) 2>/dev/null || here=$(dirname $0)
+
+# Test if we are in the directory containing RemoteMaster.jar
+if ! [ -f $here/RemoteMaster.jar ]; then
+echo "This script must be in the same directory as RemoteMaster.jar"
+exit 1
+fi
 
 # Construct paths to the new files and to the jar file. 
 desktopRMIR=$here/RMIR.desktop
@@ -30,7 +36,7 @@ Terminal=false
 Name=RMIR
 Exec=java -jar $rmprog -ir
 Type=Application
-Icon=$home/RMIR.ico
+Icon=$here/RMIR.ico
 StartupNotify=true
 Version=1.0
 EOF
@@ -54,9 +60,14 @@ chmod 775 $desktopRMIR
 chmod 775 $desktopRM
 echo Desktop files created and executable permissions set.
 
-# Test if the dialout group exists
+alldone()
+{
+echo "Setup complete."
+exit 0
+}
+
+# Test if the dialout group exists, exit with message if not
 grep -q dialout /etc/group || {
-# Group does not exist
 cat << EOF
 
 There is no user group named "dialout" in this OS, so this script does not
@@ -64,39 +75,38 @@ know how to enable a USB serial interface to be used with RMIR.  As all other
 procedures of this script have been performed, setup is now complete.
 
 EOF
-exit
+exit 0
 }
 
-# Dialout group exists, test if user is a member
-id -Gn | grep -q dialout || {
-# User is not a member, test if user is root
-if [ "$(id -u)" -ne 0 ]; then
+# Dialout group exists, test if user is a member, exit if so
+id -Gn | grep -q dialout && alldone
+
+# User is not a member, test if user is root, exit if so
+if [ "$(id -u)" -eq 0 ]; then alldone; fi
+
 # User is not root, so ask if user wishes to be added
 cat << EOF
 
 To use a USB serial interface with RMIR, you need to be a member of the
 dialout group.  Currently you are not a member.  This script can add you 
-to that group but you will need to give your sudo password.  Do you want 
+to that group but you will need to give your sudo password.  Do you want
+it to add you to that group?
+
 EOF
-echo -n "it to add you to that group?  Please answer Y or N. > "
-read reply
-temp=$(echo YyNn | grep $reply)||temp=Xx
 
 # Force user to answer Y or N (or equivalently y or n)
-while [ "$temp" = "Xx" ]; do
+reply=x
+while !  echo "YN" | grep -qi $reply; do
 echo -n "Please answer Y or N. > "
 read reply
-temp=$(echo YyNn | grep $reply)||temp=Xx
 done
 
-temp=$(echo Yy | grep $reply)||temp=Nn
-
-if [ "$temp" = "Yy" ]; then
+if echo "Y" | grep -qi $reply; then
 # User answered yes
-sudo usermod -a -G dialout $USER
+sudo usermod -aG dialout $USER
 cat << EOF
 
-Done.  You will need to log out and log back in again for this change"
+Done.  You will need to log out and log back in again for this change
 to take effect.
 
 EOF
@@ -104,15 +114,13 @@ else
 # User answered no, so tell user what to do
 cat << EOF
 
-You will need to run the command "usermod -a -G dialout $USER" to be added
+You will need to run the command "usermod -aG dialout $USER" to be added
 to the dialout group before you can use a USB serial interface with RMIR.
-After running the command, you need to log out and log back in again for this
-change to take effect.
+After running the command, you need to log out and log back in again for
+this change to take effect.
 
 EOF
 fi
 
-fi
-}
-echo "Setup complete."
+alldone
 
